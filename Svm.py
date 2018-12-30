adf0e88d0b0725beec4fdeec5479c1075de2d18c
Sanyog Vyawahare <sanvyawahare@gmail.com>
2018-12-30 14:40:41 +0530

    Update

--- Svm.py
+++ Svm.py
@@ -1,37 +1,41 @@
 from sklearn.svm import SVC
+from sklearn.multiclass import OneVsRestClassifier
 import numpy
 import pandas
 
 filename = 'WPBC.csv'
-# raw_data = open(filename,'rt')
-# reader = csv.reader(raw_data,delimiter = ',', quoting = csv.QUOTE_NONE)
 
 data = pandas.read_csv(filename,na_values = '?')
 data = data.fillna(data.mean())
-data = data[0::]  #Removed column names
+data = data[0::]  #Removed row 1 of  names
+data = numpy.array(data) 
 
-Xnum = numpy.array(data) 
-result = Xnum[:,0] #Seperated outcome column
-Xnum = numpy.delete(Xnum,(0,1),axis = 1) #Seperated other columns except OUTCOME and ID
+result = data[:,1]
+Xnum = numpy.delete(data,(0,1),axis = 1) #Seperated other columns except OUTCOME and ID
 
 
-
-from sklearn.model_selection import LeaveOneOut	#Cross Validation Leave one out method
+from sklearn.model_selection import LeaveOneOut,train_test_split	#Cross Validation Leave one out method
 loo = LeaveOneOut()
 loo.get_n_splits(Xnum)
-i = 0
-for train_index,test_index in loo.split(Xnum):
-	X_train,X_test = Xnum[train_index],Xnum[test_index]
-	y_train,y_test = result[train_index],result[test_index]
-	# X_train,X_test,y_train,y_test = train_test_split(Xnum,result,test_size =0.20) #default method of cross validation
-	print('Loop:',i)
-	i+=1
-	svclassifier = SVC(kernel = 'linear')
-	print(X_train,y_train)
-	svclassifier.fit(X_train,y_train)
 
+svclassifier =SVC(kernel = 'linear')
+# i = 0
+
+# for train_index,test_index in loo.split(Xnum):
+	
+# 	X_train,X_test = Xnum[train_index],Xnum[test_index]
+# 	y_train,y_test = result[train_index],result[test_index]
+X_train,X_test,y_train,y_test = train_test_split(Xnum,result,test_size =0.30) #default method of cross validation
+# print('Loop:',i)
+# i += 1
+svclassifier.fit(X_train,y_train)
 y_pred = svclassifier.predict(X_test)
 
-from sklearn.metric import classification_report,confusion_matrix
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
+from sklearn.metrics import classification_report,confusion_matrix  #Result and outcome
+	# print(confusion_matrix(y_test,y_pred))
+	# print(classification_report(y_test,y_pred))
+
+print("Final Result")
+y_pred = svclassifier.predict(Xnum)
+print('Confusion Matrix',confusion_matrix(result,y_pred))
+print('Report',classification_report(result,y_pred))
