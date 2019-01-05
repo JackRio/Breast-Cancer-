04b75ea537fd94c38d1bf79a0cf2d8f19374fae5
Sanyog Vyawahare <sanvyawahare@gmail.com>
2019-01-05 11:20:32 +0530

    final

--- Svm.py
+++ Svm.py
@@ -14,26 +14,25 @@ result = data[:,1]
 Xnum = numpy.delete(data,(0,1),axis = 1) #Seperated other columns except OUTCOME and ID
 
 
-from sklearn.model_selection import LeaveOneOut,train_test_split	#Cross Validation Leave one out method
+from sklearn.model_selection import LeaveOneOut	#Cross Validation Leave one out method
 loo = LeaveOneOut()
 loo.get_n_splits(Xnum)
 
 svclassifier =SVC(kernel = 'linear')
-# i = 0
+i = 0
 
-# for train_index,test_index in loo.split(Xnum):
+for train_index,test_index in loo.split(Xnum):
 	
-# 	X_train,X_test = Xnum[train_index],Xnum[test_index]
-# 	y_train,y_test = result[train_index],result[test_index]
-X_train,X_test,y_train,y_test = train_test_split(Xnum,result,test_size =0.30) #default method of cross validation
-# print('Loop:',i)
-# i += 1
-svclassifier.fit(X_train,y_train)
-y_pred = svclassifier.predict(X_test)
+	X_train = Xnum[train_index]
+	y_train = result[train_index]
+# X_train,X_test,y_train,y_test = train_test_split(Xnum,result,test_size =0.30) #default method of cross validation
+	print('Loop:',i)
+	i += 1
+	svclassifier.fit(X_train,y_train)
+
+
 
 from sklearn.metrics import classification_report,confusion_matrix  #Result and outcome
-	# print(confusion_matrix(y_test,y_pred))
-	# print(classification_report(y_test,y_pred))
 
 print("Final Result")
 y_pred = svclassifier.predict(Xnum)
